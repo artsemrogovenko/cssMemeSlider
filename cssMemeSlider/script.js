db94873ed93d9bba5541d2d2b5ec71651fd44f11
Artem_PC <taftoos2@gmail.com>
2024-09-12 20:35:07 +0300

    feat: added functionality of buttons and changing pictures (Thu, Sep 12, 2024 20:35:00 PM)

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -20,4 +20,46 @@ function parseData() {
             console.error('Ошибка при загрузке JSON:', error);
         });
 }
+window.onload = parseData;
+
+
+function initialize() {
+    let root = document.documentElement;
+    const selectors = document.querySelectorAll('.control');
+
+    controls.addEventListener('mouseover', event => {
+        const element = event.target.closest('.control_area');
+        if (element) {
+            const round = element.querySelector('.control'); // Находим дочерний элемент .control
+            round.classList.add('hover');
+        }
+    });
+    controls.addEventListener('mouseout', event => {
+        const element = event.target.closest('.control_area');
+        if (element) {
+            const round = element.querySelector('.control'); // Находим дочерний элемент .control
+            round.classList.remove('hover');
+        }
+    });
+
+    controls.addEventListener('click', event => {
+        const element = event.target.closest('.control_area');
+        if (element) {
+            selectors.forEach(selector => {
+                selector.classList.remove('control_active');
+            });
+            const id = element.id;
+            const mem = slidesData[id - 1];
+            const round = element.querySelector('.control');
+            round.classList.add('control_active');
+            slider.innerHTML = `<img src="../cssMemeSlider/assets/${slidesData[id - 1]['img']}" alt="img"></img>`;
+            caption.innerHTML = mem['description'];
+            root.style.setProperty('--slider-bg-color', mem['bgcolor']);
+        }
+    });
+}
+
+
+
+
 
