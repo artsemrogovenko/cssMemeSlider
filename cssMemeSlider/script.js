b9f95899375d0cdf1585d87b2e4986be4bf498ab
Artem_PC <taftoos2@gmail.com>
2024-09-13 17:04:36 +0300

    feat: added gradual change of images and text (Fri, Sep 13, 2024 17:04:35 PM)

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -1,6 +1,9 @@
 const slider = document.querySelector('.slider');
 const controls = document.querySelector('.controls');
 const caption = document.querySelector('.caption');
+let slider_width = slider.getBoundingClientRect().width; // ширина слайдера
+let drawed = false
+let root = document.documentElement;
 let currentSlide = 0;
 let slidesData = [];
 
@@ -22,10 +25,24 @@ function parseData() {
 }
 window.onload = parseData;
 
+let resizeTimeout;
+
+window.addEventListener('resize', () => {
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(() => {
+        slider_width = slider.getBoundingClientRect().width;
+        if (drawed) {
+            const images = document.querySelectorAll('img');
+            images.forEach((image) => {
+                updateStyle(image);
+            });
+        }
+    }, 50); 
+});
+const selectors = document.querySelectorAll('.control');
 
 function initialize() {
-    let root = document.documentElement;
-    const selectors = document.querySelectorAll('.control');
+    drawAllcards();
 
     controls.addEventListener('mouseover', event => {
         const element = event.target.closest('.control_area');
@@ -52,14 +69,39 @@ function initialize() {
             const mem = slidesData[id - 1];
             const round = element.querySelector('.control');
             round.classList.add('control_active');
-            slider.innerHTML = `<img src="../cssMemeSlider/assets/${slidesData[id - 1]['img']}" alt="img"></img>`;
-            caption.innerHTML = mem['description'];
+            // slider.innerHTML = `<img src="../cssMemeSlider/assets/${slidesData[id - 1]['img']}" alt="img"></img>`;
+            root.style.setProperty('--offset',`-${slider_width*(id-1)}px`);
             root.style.setProperty('--slider-bg-color', mem['bgcolor']);
+            caption.style.opacity='0';
+            setTimeout(() => {
+                caption.textContent =  mem['description'];
+                caption.style.opacity='1';
+            }, 500); // Время должно совпадать с продолжительностью перехода
         }
     });
 }
 
 
+function drawAllcards(){
+    for (let i = 0; i < slidesData.length; i++) {
+        const meme = document.createElement('img');
+        meme.src = `../cssMemeSlider/assets/${slidesData[i]['img']}`;
+        meme.alt = 'img';
+        slider.appendChild(meme);
+        meme.onload = ()=>{
+            updateStyle(meme);
+        }
+    }
+    if(!drawed){
+        caption.textContent =  slidesData[0]['description'];
+        selectors[0].classList.add('control_active');
+    }
+    drawed=true;
+}
 
-
-
+function updateStyle(element){
+    const image_width = element.clientWidth; // ширина отображения в слайдере
+    const margin= (slider_width - image_width) / 2; // оступ с края
+    element.style.marginRight = `${margin}px`;
+    element.style.marginLeft = `${margin}px`;
+}
